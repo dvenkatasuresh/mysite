dd49507ce007486e92b8039db30737b5ffcd672d
dvenkatasuresh <vdaggupa@kent.edu>
2022-10-26 23:01:22 +0000

    final changes

--- bottle_app.py
+++ bottle_app.py
@@ -1,40 +1,49 @@
 from bottle import default_app, route, get, post, template, request, redirect
 
-import database
+from peewee_database import get_items, add_item, delete_item, update_item
 
 @route('/')
 def get_index():
     redirect('/list')
+    #return "Hey you!"
 
 @route('/list')
 def get_list():
-    items = database.get_items()
+    items = get_items()
+    #items={'acs','sdf'}
+        #return "acha"
     return template("shoppinglist.tpl", name="DV Suresh", shopping_list=items)
 
 @post('/add')
 def post_add():
     description = request.forms.get("description")
-    database.add_item(description)
+    # quantity = request.forms.get("quantity")
+    try:
+        quantity = int(quantity)
+    except:
+        quantity = 1
+    # add_item(description, quantity)
+    add_item(description)
     redirect('/list')
 
 @route("/delete/<id>")
 def get_delete(id):
-    database.delete_item(id)
+    delete_item(id)
     redirect('/list')
 
 @get("/edit/<id>")
 def get_edit(id):
-    items = database.get_items(id)
+    items = get_items(id)
     if len(items) != 1:
         redirect('/list')
-    item_id, description = items[0]['id'], items[0]['desc']
+    item_id, description = items[0]['id'], items[0]['description']
     assert item_id == int(id)
     return template("edititem.tpl", id=id, description=description)
 
 @post("/edit/<id>")
 def post_edit(id):
     description = request.forms.get("description")
-    database.update_item(id, description)
+    update_item(id, description)
     redirect('/list')
 
 application = default_app()
